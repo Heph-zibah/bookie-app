4bcf0027aeea7cd16ffdcf594296a2c503871805
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-13 14:38:08 +0100

    added the global styles and navbar path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import Navbar from './Components/Navbar/Navbar';
+const GlobalStyle = createGlobalStyle`
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  list-style-type:none;
+}
+:root{
+  --clr-primary: #1ABCCA;
+  --clr-secondary:#DFE2E2;
+  --clr-text-primary:#595757;
+  --bg-primary:#F5F5F5;
+}
+
+body{
+  display:flex;
+  flex-direction:column;
+  background:var(--bg-primary);
+  font-family: 'Outfit', sans-serif;
+  font-weight:400;
+  font-size:1rem;
+  line-height:1.5;
+  width:100%;
+}`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Navbar />
+		</>
+	);
 }
 
 export default App;
