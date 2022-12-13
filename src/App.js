3726d21cfc266f44c455cf6c5fb5727d4cb3483c
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-13 15:30:01 +0100

    removed styled component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
+import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
-const GlobalStyle = createGlobalStyle`
-* {
-  margin:0;
-  padding:0;
-  box-sizing:border-box;
-  list-style-type:none;
-}
-:root{
-  --clr-primary: #1ABCCA;
-  --clr-secondary:#DFE2E2;
-  --clr-text-primary:#595757;
-  --bg-primary:#F5F5F5;
-}
-
-body{
-  display:flex;
-  flex-direction:column;
-  background:var(--bg-primary);
-  font-family: 'Outfit', sans-serif;
-  font-weight:400;
-  font-size:1rem;
-  line-height:1.5;
-  width:100%;
-}`;
 
 function App() {
 	return (
 		<>
-			<GlobalStyle />
 			<Navbar />
+			<Hero />
 		</>
 	);
 }
