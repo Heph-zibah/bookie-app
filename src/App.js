161edbf3abe7f1970851b0a6ce497c1c7a16311c
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-13 15:59:40 +0100

    added services section path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
+import Services from './Components/ServiceSection/Services';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Services />
 		</>
 	);
 }
