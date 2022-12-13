1d3b44ceffefd0335c76997a45a4711f73d6da46
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-13 16:00:50 +0100

    added the markup

---
+++ src/Components/ServiceSection/Services.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import servicesImg from '../../Assets/services-img.svg';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import './Services.css';
+
+const Services = () => {
+	return (
+		<>
+			<section className='wrapper'>
+				<div className='services__container'>
+					<h1>The Services We Provide </h1>
+					<div className='services__content'>
+						<div className='services__img'>
+							<img src={servicesImg} alt='happy woman holding a ball' />
+						</div>
+						<div className='services__info'>
+							<h2>Save Money </h2>
+							<p>You can have a saving wallets with Bookie</p>
+							<button>
+								Learn More <MdKeyboardArrowRight />
+							</button>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Services;
