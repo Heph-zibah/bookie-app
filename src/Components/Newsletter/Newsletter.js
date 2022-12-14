712db4fa97af78367f799b607b1cca56fdc32be3
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-14 21:34:45 +0100

    created the folder and added the styles and markup

---
+++ src/Components/Newsletter/Newsletter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import newsImg from '../../Assets/new-img.svg';
+import './Newsletter.css';
+
+const Newsletter = () => {
+	return (
+		<>
+			<section className='news__container'>
+				<div className='news__content'>
+					<div className='news__img'>
+						<img src={newsImg} alt='a woman reading to kids in a library' />
+					</div>
+					<div className='news__info'>
+						<h1>
+							New to <span> Bookie</span>
+						</h1>
+						<p>Subscribe to our newletter to get updates on our latest offers. </p>
+						<form className='news__form'>
+							<input type='text' placeholder='Enter Email' />
+							<div>
+								<button>Subscribe</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Newsletter;
