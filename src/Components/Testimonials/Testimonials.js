1cec2187be8f3f16ce770afead9e84def41ab63a
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-15 07:50:42 +0100

    added the markup

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import avatar from '../../Assets/avatar-testimonials.svg';
-import './Testimonials.css'
+import './Testimonials.css';
 
 const Testimonials = () => {
 	return (
 		<>
-			<section>
-				<div></div>
+			<section className='testimonials__container'>
+				<div className='testimonials__content'>
+					<h1>Testimonials</h1>
+					<div className='testimonials__cards'>
+						<div className='testimonials__card'>
+							<img src={avatar} alt='avatar' />
+							<p>
+								“Bookie is a very easy platform which allow users, save money ,buy books
+								and other educational Material ”
+							</p>
+							<strong>-Adewale 0</strong>
+						</div>
+					</div>
+				</div>
 			</section>
 		</>
 	);
