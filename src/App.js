cf720e3bc009a6d810138160b60e8a3066cac354
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-17 16:44:50 +0100

    added path to the auth folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Hero from './Components/Hero/Hero';
-import Navbar from './Components/Navbar/Navbar';
-import Services from './Components/ServiceSection/Services';
-import Buy from './Components/BuySection/Buy';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Footer from './Components/Footer/Footer';
-import Newsletter from './Components/Newsletter/Newsletter';
-import Books from './Components/Books/Books';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ForgotPassword1 from './Components/Auth/ForgotPassword1';
+import ForgotPassword2 from './Components/Auth/ForgotPassword2';
+import LogIn from './Components/Auth/LogIn';
+import SignUp from './Components/Auth/SignUp';
+import Verify from './Components/Auth/Verify';
+import Home from './Pages/Home';
 
 function App() {
 	return (
-		<>
-			<Navbar />
-			<Hero />
-			<Services />
-			<Buy />
-			<Books />
-			<Testimonials />
-			<Newsletter />
-			<Footer />
-		</>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/SignUp' element={<SignUp />} />
+				<Route path='/Verify' element={<Verify />} />
+				<Route path='/LogIn' element={<LogIn />} />
+				<Route path='/ForgotPassword1' element={<ForgotPassword1 />} />
+				<Route path='/ForgotPassword2' element={<ForgotPassword2 />} />
+			</Routes>
+		</Router>
 	);
 }
 
