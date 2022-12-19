dc24beb56f15c45f47fbe2f0c96a2942cd7c67f1
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-12-19 01:31:20 +0100

    created about page

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Components/Footer/Footer';
+import Navbar from '../Components/Navbar/Navbar';
+
+const About = () => {
+	return (
+		<>
+			<Navbar />
+			<Footer />
+		</>
+	);
+};
+
+export default About;
